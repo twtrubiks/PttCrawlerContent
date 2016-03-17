55f2825c70c9e5de6cb206ec42fa6ad0ec984828
twtrubiks <twtrubiks@gmail.com>
2016-03-17 21:41:16 +0800

    Improve performance

--- PttCrawler.py
+++ PttCrawler.py
@@ -9,9 +9,9 @@ from bs4 import BeautifulSoup
 requests.packages.urllib3.disable_warnings()
 
 
-PttName=''
+Board = ''
 payload={
-'from':'/bbs/'+PttName+'/index.html',
+'from':'/bbs/'+ Board +'/index.html',
 'yes':'yes' 
 }
 
@@ -21,59 +21,51 @@ def getPageNumber(content) :
     pageNumber = content[startIndex+5 : endIndex]
     return pageNumber
 
-# pyhton PttCrawler.py AKB48 2 
+# python PttCrawler.py Gossiping 2 
 if __name__ == "__main__":
-    
-    #PttName = raw_input('請輸入版名：')
-    PttName = str(sys.argv[1])
+    Board = str(sys.argv[1])
     ParsingPage = int(sys.argv[2])
-    print 'Start parsing [',PttName,']....'
+    print 'Start parsing ['+ Board +']....'
     start_time = time.time()
-    rs=requests.session()
-    res=rs.post('https://www.ptt.cc/ask/over18',verify=False,data=payload)
-    res=rs.get('https://www.ptt.cc/bbs/'+PttName+'/index.html',verify=False)
-    soup=BeautifulSoup(res.text,'html.parser')
+    rs = requests.session()
+    #八卦版18禁
+    res = rs.post('https://www.ptt.cc/ask/over18',verify = False, data = payload)
+    res = rs.get('https://www.ptt.cc/bbs/'+ Board +'/index.html',verify = False)
+    soup = BeautifulSoup(res.text,'html.parser')
     ALLpageURL = soup.select('.btn.wide')[1]['href']
-    ALLpage=int(getPageNumber(ALLpageURL))+1
-    print 'Total pages:',ALLpage
-    obj=[]
+    ALLpage = int(getPageNumber(ALLpageURL)) + 1
+    print 'Total pages:', ALLpage
     URLlist=[]
-    FILENAME='PttData-'+PttName+'-'+datetime.now().strftime('%Y-%m-%d-%H-%M-%S')+'.txt'
-    file = open(FILENAME, 'a')  
+    fileName='PttData-'+ Board + '-' + datetime.now().strftime('%Y%m%d%H%M%S')+'.txt'
 
-    for number in range(ALLpage, ALLpage-int(ParsingPage),-1):
-        url = 'https://www.ptt.cc/bbs/'+PttName+'/index'+str(number)+'.html'
-        #print 'page:',number
-        res=rs.get(url,verify=False)
-        soup=BeautifulSoup(res.text,'html.parser')
-        UrlPer=[]
+    #得到每頁的所有文章
+    for index in range(ALLpage, ALLpage-int(ParsingPage), -1):
+        url = 'https://www.ptt.cc/bbs/'+ Board +'/index'+ str(index) +'.html'
+        res = rs.get(url, verify = False)
+        soup = BeautifulSoup(res.text,'html.parser')
+        UrlPer = []
         for entry in soup.select('.r-ent'):
-            atag=entry.select('.title')[0].find('a') 
-	    if(atag!=None):
-               URL=atag['href']   
-               #print 'URL:',URL          
-               UrlPer.append('https://www.ptt.cc'+URL)   
-                     
-        for listURL in reversed(UrlPer):
-            URLlist.append(listURL)
-            #print 'url:',listURL
+            atag = entry.select('.title')[0].find('a') 
+	    if(atag != None):
+               URL = atag['href']          
+               UrlPer.append('https://www.ptt.cc' + URL)   
+        #需要反轉,因為網頁版最下面才是最新的文章
+        for URL in reversed(UrlPer):
+            URLlist.append(URL)   
     
-    
-    strNext=u"\n\n\n\n***************下一篇***************\n\n\n\n\n";
+    strNext = u"\n\n\n\n***************下一篇***************\n\n\n\n\n";
+    content = ''
+    #得到每篇文章的內容
     for URL in URLlist:
-        res=rs.get(URL,verify=False)
-        soup=BeautifulSoup(res.text,'html.parser')
-        content=soup.select('.bbs-screen.bbs-content')[0].text
-        obj.append(content+strNext)
+        res = rs.get(URL, verify = False)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        data = soup.select('.bbs-screen.bbs-content')[0].text
+        content += (data + strNext)
         time.sleep(0.05) 
-    
-    
-    line=''
-    for data in obj:          
-        line+=data
-    file.write(line.encode('utf8'))
-    file.close()
-      
-    print '====================END===================='
-    print 'execution time:' + str(time.time() - start_time)+'s'
+       
+    with open(fileName,'wb') as f:
+        f.write( content.encode('utf8') )  
+     
+    print u'====================END===================='
+    print u'execution time:' + str(time.time() - start_time)+'s'
     
